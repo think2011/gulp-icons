798ee31d0668f65f0e082952bb2273012d0c0071
think2011 <452125301@qq.com>
2017-03-31 15:21:05 +0800

    perf 🎯 解决因小数精度问题导致图片边角缺失

--- gulpfile.js
+++ gulpfile.js
@@ -26,25 +26,42 @@ gulp.task('icons:sprite', async (cb) => {
                     imgName: `${name}.png`,
                     cssName: `${name}.scss`,
                     cssTemplate(sprite){
+                        /*
+                         * 加大单位的数值解决浏览器对小数解析精度导致图片边角缺失的问题
+                         */
                         let prevStr = `
-                        /* rem */
-                        $designWidth: 750;
-                        @function px2rem($px) {
-                        @return $px*320/$designWidth/20 + rem;
-                        }
-                        
-                        .icon {
-                            display:inline-block;
-                        }
+/* rem */
+$designWidth: 750;
+@function _px($px) {
+  @return $px*320/$designWidth/20 + rem;
+}
+
+.icon {
+  display: inline-block;
+  position: relative;
+}
                         `
                         let cssItem = sprite.sprites.map((item) => {
                             return `
-                            .${name}-${item.name} {
-                                width: px2rem(${item.width});
-                                height: px2rem(${item.height});
-                                background: url(${item.escaped_image}) no-repeat px2rem(-${item.x}) px2rem(-${item.y});
-                                background-size: px2rem(${item.total_width}) px2rem(${item.total_height});
-                            }
+.${name}-${item.name} {
+  width: _px(${item.width});
+  height: _px(${item.height});
+
+  &:after {
+    content: '';
+    width: 10000%;
+    height: 10000%;
+    position: absolute;
+    left: 0;
+    top: 0;
+    background: url(icon-index.png) no-repeat _px(-${item.x * 100}) _px(-${item.y * 100});
+    background-size: _px(${item.total_width * 100}) _px(${item.total_height * 100});
+    -webkit-transform-origin: 0 0;
+    -webkit-transform: scale(.01);
+    transform-origin: 0 0;
+    transform: scale(.01);
+  }
+}
                             `
                         })
 
